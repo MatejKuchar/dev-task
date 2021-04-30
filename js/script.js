1dc0319e548f2ae2c0f27edab12f9f18e5d231a1
MatejKuchar <matej.kuchar01@gmail.com>
2021-04-30 12:48:26 +0200

    added gradient transition on about circles and courses cards and stop scrolling if menu is open

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const menu = document.querySelector(".navbar__content");
 const menuOpen = document.querySelector(".navbar__open");
 const menuClose = document.querySelector(".navbar__close");
+const bodyElement = document.body;
 
 menuOpen.addEventListener("click", (e) => {
   menu.classList.add("active");
   menuOpen.classList.add("hide");
+  bodyElement.classList.add("stop-scrolling");
 });
+
 menuClose.addEventListener("click", (e) => {
   menu.classList.remove("active");
   menuOpen.classList.remove("hide");
+  bodyElement.classList.remove("stop-scrolling");
 });
